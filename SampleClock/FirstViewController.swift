4a22764bb2070264f7092fbb08287adf7f91db43
Kazuki Hamada <kazuki.ma9376@gmail.com>
2019-12-15 19:45:08 +0900

    Initial Commit

---
+++ SampleClock/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SampleClock
+//
+//  Created by 濱田一輝 on 2019/12/15.
+//  Copyright © 2019 Kazuki Hamada. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleClock/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SampleClock
+//
+//  Created by 濱田一輝 on 2019/12/15.
+//  Copyright © 2019 Kazuki Hamada. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
